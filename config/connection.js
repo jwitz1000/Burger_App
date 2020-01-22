46616a74ba29e3c45b278926710daa66313f7bdd
Arman Riahi <namrataffy@gmail.com>
2020-01-22 14:54:37 -0800

    added db variable

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "xbgm3ff9560etok1",
-  password: "lzstidu88a5uwh4y",
-  database: "zbuvfx2sxef4nadu	"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if (err) {
