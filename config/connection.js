ba804fb7450d5b6a688b7e54865dda9fbf476567
Arman Riahi <namrataffy@gmail.com>
2020-01-22 14:52:43 -0800

    changed config for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "xbgm3ff9560etok1",
+  password: "lzstidu88a5uwh4y",
+  database: "zbuvfx2sxef4nadu	"
 });
 
 connection.connect(function(err) {
